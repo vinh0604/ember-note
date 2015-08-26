e235f54a1c21f0f91721aa660ceffde8e5526c2c
vinhbachsy <bsdvinhuit@gmail.com>
2015-08-26 19:03:18 +0800

    Update UI

--- app/helpers/marked.js
+++ app/helpers/marked.js
@@ -2,5 +2,5 @@ import marked from 'npm:marked'
 import Ember from 'ember'
 
 export default Ember.Handlebars.helper('marked', (value, options) => {
-    return value || marked(value)
+    return value ? marked(value) : ''
 })
